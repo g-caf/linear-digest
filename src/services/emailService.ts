b9ab5f5aac9655324b5f34a9972492ed88fe4395
Adrienne Adrienne <adrienne.caffarel@Adriennes-MBP.lan>
2026-01-13 11:31:42 -0500

    Lazy init Twilio/SendGrid clients to avoid startup crash

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,9 +1,17 @@
 import sgMail from '@sendgrid/mail';
 import { env } from '../config/env.js';
 
-sgMail.setApiKey(env.SENDGRID_API_KEY);
+let initialized = false;
+
+function initSendGrid() {
+  if (!initialized) {
+    sgMail.setApiKey(env.SENDGRID_API_KEY);
+    initialized = true;
+  }
+}
 
 export async function sendDigestEmail(to: string, subject: string, htmlBody: string): Promise<void> {
+  initSendGrid();
   await sgMail.send({
     to,
     from: env.EMAIL_FROM,
--- src/services/smsService.ts
+++ src/services/smsService.ts
@@ -1,10 +1,17 @@
 import twilio from 'twilio';
 import { env } from '../config/env.js';
 
-const client = twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+let client: ReturnType<typeof twilio> | null = null;
+
+function getClient() {
+  if (!client) {
+    client = twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+  }
+  return client;
+}
 
 export async function sendDigestSms(to: string, body: string): Promise<void> {
-  await client.messages.create({
+  await getClient().messages.create({
     to,
     from: env.TWILIO_FROM_NUMBER,
     body,
